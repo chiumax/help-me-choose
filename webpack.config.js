aa2b85fcb27c3c3921ac19e5270fc4d7318d9875
dumblole <move2234@gmail.com>
2018-08-06 18:11:03 -0400

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,30 @@
 const path = require('path');
 
+module.exports = {
+	entry: './src/app.jsx',
+	output: {
+		path: __dirname,
+		filename: 'bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				loader: 'babel-loader',
+				test: /\.jsx$/,
+				exclude: /node_modules/
+			},
+			{
+				test: /\.s?css$/,
+				use: ['style-loader', 'css-loader', 'sass-loader']
+			}
+		]
+	},
+	devtool: 'source-map',
+	devServer: {
+		contentBase: __dirname
+	}
+};
+/**
 module.exports = env => {
 	return {
 		entry: './src/app.jsx',
@@ -29,3 +54,4 @@ module.exports = env => {
 		}
 	};
 };
+**/
