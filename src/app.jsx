1c14ede63eec24a358dde33fef7a4ef9afb40206
dumblole <move2234@gmail.com>
2018-08-11 15:14:12 -0400

    updated button animation not applying to every single button

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,12 @@
 // import validator from 'validator';
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import 'normalize.css/normalize.css';
-import './styles/styles.scss';
+import "normalize.css/normalize.css";
+import "./styles/styles.scss";
 
-import HelpMeChooseApp from './components/HelpMeChooseApp.jsx';
-console.log('hello');
+import HelpMeChooseApp from "./components/HelpMeChooseApp.jsx";
 
-ReactDOM.render(<HelpMeChooseApp />, document.getElementById('app'));
+ReactDOM.render(<HelpMeChooseApp />, document.getElementById("app"));
 // babel src/app.jsx --out-file=scripts/app.js --presets=env,react --watch
 // live-server
--- src/components/AddOption.jsx
+++ src/components/AddOption.jsx
@@ -1,20 +1,20 @@
-import React from 'react';
+import React from "react";
 
 const AddOption = props => {
-	return (
-		<div>
-			{console.log(props.error)}
-			{!!props.error && <p className="add-option-error">{props.error}</p>}
-			<form onSubmit={props.handleAddOption} className="add-option">
-				<input
-					type="text"
-					name="submit"
-					placeholder="List option here"
-					className="add-option__input"
-				/>
-				<button className="button">Submit</button>
-			</form>
-		</div>
-	);
+  return (
+    <div>
+      {console.log(props.error)}
+      {!!props.error && <p className="add-option-error">{props.error}</p>}
+      <form onSubmit={props.handleAddOption} className="add-option">
+        <input
+          type="text"
+          name="submit"
+          placeholder="List option here"
+          className="add-option__input"
+        />
+        <button className="button button-org">Submit</button>
+      </form>
+    </div>
+  );
 };
 export { AddOption as default };
