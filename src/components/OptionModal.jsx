198bec744d1618e641563443eadccb82069bafbf
dumblole <move2234@gmail.com>
2018-08-24 13:56:46 -0400

    updated button animation

--- src/components/OptionModal.jsx
+++ src/components/OptionModal.jsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import Modal from 'react-modal';
+import React from "react";
+import Modal from "react-modal";
 
 const OptionModal = props => {
-	return (
-		<Modal
-			isOpen={!!props.selectedOption}
-			contentLabel="Selected Option"
-			ariaHideApp={false}
-			onRequestClose={props.handleModalVis}
-			closeTimeoutMS={200}
-			className="modal"
-		>
-			<h3 className="modal__title">Selected Option</h3>
-			{props.selectedOption && (
-				<p className="modal__body">{props.selectedOption}</p>
-			)}
-			<button className="button" onClick={props.handleModalVis}>
-				Okay
-			</button>
-		</Modal>
-	);
+  return (
+    <Modal
+      isOpen={!!props.selectedOption}
+      contentLabel="Selected Option"
+      ariaHideApp={false}
+      onRequestClose={props.handleModalVis}
+      closeTimeoutMS={200}
+      className="modal"
+    >
+      <h3 className="modal__title">Selected Option</h3>
+      {props.selectedOption && (
+        <p className="modal__body">{props.selectedOption}</p>
+      )}
+      <button className="button button-link" onClick={props.handleModalVis}>
+        Okay
+      </button>
+    </Modal>
+  );
 };
 
 export { OptionModal as default };
